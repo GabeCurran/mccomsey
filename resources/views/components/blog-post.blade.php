37971ee15b433a32d9a033fac8d93d982088b18f
Spencer Heffley <spheffley@yahoo.com>
2022-03-23 10:26:30 -0400

    Changed selectors for blog post styling to only effect blog posts

--- resources/views/components/blog-post.blade.php
+++ resources/views/components/blog-post.blade.php
@@ -1,36 +1,36 @@
 <div class="mt-4 p-2 bg-gray-100 border rounded-lg">
     <h3 class='border-b mb-2 text-3xl'>{{ $title }}</h3>
-    <div>{{ $post_content }}</div>
+    <div class='blog-content'>{{ $post_content }}</div>
 </div>
 <script>
-    heading1s = document.getElementsByTagName('h2');
+    heading1s = document.querySelectorAll('.blog-content h2');
     for (i = 0; i < heading1s.length; i++) {
         heading1s[i].classList.add('text-2xl');
     }
-    heading2s = document.getElementsByTagName('h3');
+    heading2s = document.querySelectorAll('.blog-content h3');
     for (i = 0; i < heading2s.length; i++) {
         heading2s[i].classList.add('text-xl');
     }
-    heading3s = document.getElementsByTagName('h4');
+    heading3s = document.querySelectorAll('.blog-content h4');
     for (i = 0; i < heading3s.length; i++) {
         heading3s[i].classList.add('text-lg');
     }
-    bulletList = document.getElementsByTagName('ul');
+    bulletList = document.querySelectorAll('.blog-content ul');
     for (i = 0; i < bulletList.length; i++) {
         bulletList[i].classList.add('list-disc');
         bulletList[i].classList.add('ml-4');
     }
-    orderedList = document.getElementsByTagName('ol');
+    orderedList = document.querySelectorAll('.blog-content ol');
     for (i = 0; i < orderedList.length; i++) {
         orderedList[i].classList.add('list-decimal');
         orderedList[i].classList.add('ml-4');
     }
-    links = document.getElementsByTagName('a');
+    links = document.querySelectorAll('.blog-content a');
     for (i = 0; i < links.length; i++) {
         links[i].classList.add('text-blue-500');
         links[i].classList.add('underline');
     }
-    blockquotes = document.getElementsByTagName('blockquote');
+    blockquotes = document.querySelectorAll('.blog-content blockquote');
     for (i = 0; i < blockquotes.length; i++) {
         blockquotes[i].classList.add('border-l-4');
         blockquotes[i].classList.add('border-gray-300');
@@ -38,20 +38,20 @@
         blockquotes[i].classList.add('ml-4');
         blockquotes[i].classList.add('mb-4');
     }
-    tables = document.getElementsByTagName('table');
+    tables = document.querySelectorAll('.blog-content table');
     for (i = 0; i < tables.length; i++) {
         tables[i].classList.add('table-auto');
         tables[i].classList.add('border-collapse');
         tables[i].classList.add('ml-4');
         tables[i].classList.add('mb-4');
     }
-    tableCell = document.getElementsByTagName('td');
+    tableCell = document.querySelectorAll('.blog-content td');
     for (i = 0; i < tableCell.length; i++) {
         tableCell[i].classList.add('border-4');
         tableCell[i].classList.add('border-gray-300');
         tableCell[i].classList.add('p-4');
     }
-    media = document.getElementsByTagName('oembed');
+    media = document.querySelectorAll('oembed');
     for (i = 0; i < media.length; i++) {
         media[i].classList.add('width-full');
     }
