6336947abd49e6e5abc9b4986741a4162e673a92
Spencer Heffley <spheffley@yahoo.com>
2022-03-25 09:17:22 -0400

    Added text to banner

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,2 +1,7 @@
 <!--<img src='http://placekitten.com/g/2000/600' alt='placeholder'>-->
-<img src='images/banner.png' alt='outside of mccomsey automotive'>
+<figure class='relative'>
+    <img src='images/banner.png' alt='outside of mccomsey automotive'>
+    <figcaption class='absolute bottom-0 left-0 backdrop-brightness-75 w-full'>
+        <p class='text-8xl text-white font-bold font-sans p-2'>Auto Care <br /> By those who care</p>
+    </figcaption>
+</figure>
