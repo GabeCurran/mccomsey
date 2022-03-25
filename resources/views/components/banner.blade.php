0fdf15981c81950d890226a4d4efdbc61136cefc
Spencer Heffley <spheffley@yahoo.com>
2022-03-25 11:40:52 -0400

    Parallax

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,6 +1,7 @@
 <!--<img src='http://placekitten.com/g/2000/600' alt='placeholder'>-->
-<figure class='relative'>
-    <img src='images/banner.png' alt='outside of mccomsey automotive'>
+<figure class='relative parallax'>
+    <!--<img src='images/banner.png' alt='outside of mccomsey automotive'>-->
+    <div class='parallax'></div> 
     <figcaption class='absolute bottom-0 left-0 backdrop-brightness-65 w-full'>
         <p class='text-[6vw] leading-none text-white font-bold font-sans p-4'>McComsey <br /> Automotive</p>
     </figcaption>
