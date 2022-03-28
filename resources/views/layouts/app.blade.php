8812f77d28b4063a480066fc453ea81b18e63bf1
Spencer Heffley <spheffley@yahoo.com>
2022-03-28 09:16:35 -0400

    Added scrolling parallax effect to the banner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,10 +18,13 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script src="{{ asset('js/footer.js') }}" defer></script>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="{{ 'js/parallax.min.js' }}" defer></script>
     </head>
     <body class="font-sans antialiased">
+        <x-banner class='w-screen banner'></x-banner>
         <div class="min-h-screen bg-white content-container">
-                <x-banner class='w-screen'></x-banner>
                 @include('layouts.navigation')
 
             <!-- Page Heading -->
