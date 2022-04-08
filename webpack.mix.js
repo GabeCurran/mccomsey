2b0f2634c337af3267df38d550b23a8bede3735f
Spencer Heffley <spheffley@yahoo.com>
2022-04-08 08:23:40 -0400

    Deleted images and added js.map

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,3 +20,7 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
 mix.scripts([
     'node_modules/ckeditor5-build-laravel-image/build/ckeditor.js',
   ], 'public/js/vendors.js');
+
+  mix.scripts([
+    'node_modules/ckeditor5-build-laravel-image/build/ckeditor.js.map',
+  ], 'public/js/ckeditor.js.map');
