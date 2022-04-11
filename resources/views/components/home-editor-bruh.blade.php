975f036114ddc22754f488f0e1df3f890613f10f
Gabe <gabecurran01@gmail.com>
2022-04-11 09:27:01 -0400

    no shadows

--- resources/views/components/home-editor-bruh.blade.php
+++ resources/views/components/home-editor-bruh.blade.php
@@ -1,6 +1,6 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="bg-white overflow-hidden">
             <div class="p-6 bg-white border-b border-gray-200">
 
                 {{-- <div class="mb-4">
@@ -16,7 +16,7 @@
                 </div>
 
                 <div class="flex p-1">
-                    <button type="submit" role="submit" class="p-3 bg-blue-500 text-white hover:bg-blue-400" required>Submit</button>
+                    <button type="submit" role="submit" class="mt-3 ml-5 p-3 bg-blue-500 text-white hover:bg-blue-400" required>Submit</button>
                 </div>
             </div>
         </div>
