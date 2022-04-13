89b344818bb497b5ee7a6138934d303f91be6490
Spencer Heffley <spheffley@yahoo.com>
2022-04-13 10:46:14 -0400

    Added real email adress and added links to click for phone and email

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,9 +27,9 @@
                 <p>11 E Lancaster Ave</p>
                 <p>Oxford, PA 19363</p>
                 <p class='font-bold'>Phone:</p>
-                <p>(610) 467-1330</p>
+                <a href='tel:610-467-1330' class='hover:underline'>(610) 467-1330</a>
                 <p class='font-bold'>Email:</p>
-                <p>mccomsey@auto.com</p>
+                <a href='mailto:mccomseyautomotive@gmail.com' class='hover:underline'>mccomseyautomotive@gmail.com</a>
             </div>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3065.9157791078887!2d-75.98286888451125!3d39.78644540165321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c7b3511362f155%3A0x56b693145e67edef!2sMcComsey%20Automotive!5e0!3m2!1sen!2sus!4v1648650972285!5m2!1sen!2sus" class='w-2/3 h-auto basis-2/3 rounded-lg border border-black' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
