6fa23ffa1bbfef68ebd58b5617eb819bc278bacd
Spencer Heffley <spheffley@yahoo.com>
2022-04-13 09:45:39 -0400

    Added some text to the appointments page

--- resources/views/appointments.blade.php
+++ resources/views/appointments.blade.php
@@ -1,7 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Request Appointment') }}
+            {{ __('Appointments') }}
         </h2>
+    </x-slot>
+
+    <x-slot name='content1'>
+        <h1 class='text-4xl mb-5'>Upcoming Appointments</h1>
+        <p>You have no upcoming appointments</p>
     </x-slot>
 </x-app-layout>
