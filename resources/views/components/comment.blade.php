987254c328ab03a4efd7a0a906d079f704b99066
Spencer Heffley <spheffley@yahoo.com>
2022-04-22 15:59:45 -0400

    Changed min width of profile picture so it does not get squeeshed

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,7 +1,7 @@
 <div class='flex divide-y'>
     <div class='ml-10 border border-grey-300'></div>
     <div class="mt-4 ml-5 p-2 w-[90%] flex">
-        <div class='w-12 h-12 rounded-full bg-gray-200 mr-2 bg-blue-400 relative overflow-hidden'>
+        <div class='w-12 h-12 min-w-[3rem] rounded-full bg-gray-200 mr-2 bg-blue-400 relative overflow-hidden'>
             <div class='w-4 h-4 rounded-full bg-blue-300 absolute top-2 left-4'></div>
             <div class='w-2 h-2 bg-blue-300 absolute top-5 left-5'></div>
             <div class='w-6 h-8 rounded-full bg-blue-300 absolute top-6 left-3'></div>
