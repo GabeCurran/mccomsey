b0c4c36bc4a8a6f7dfa801388428a8b56380ddbd
Spencer Heffley <spheffley@yahoo.com>
2022-05-04 08:44:24 -0400

    Styled the appointments page

--- resources/views/appointments.blade.php
+++ resources/views/appointments.blade.php
@@ -6,42 +6,39 @@
     </x-slot>
 
     <x-slot name='content1'>
-        <h1 class='text-4xl mb-5'>Upcoming Appointments</h1>
-        <p>You have no upcoming appointments</p>
+        <div class='flex flex-col max-w-lg m-auto'>
+            <h1 class='text-4xl mb-5'>Upcoming Appointments</h1>
+            <p>You have no upcoming appointments</p>
 
-        <h1 class='text-4xl my-5'>Request an Appointment</h1>
-        <form method='post' action=''>
-            @csrf
-            <div class='mb-5'>
-                <label for='name' class='block text-gray-700 text-sm font-bold mb-2'>
-                    Name
-                </label>
-                <input type='text' name='name' id='name' class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Name'>
-            </div>
-            <div class='mb-5'>
-                <label for='email' class='block text-gray-700 text-sm font-bold mb-2'>
-                    Email
-                </label>
-                <input type='email' name='email' id='email' class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Email'>
-            </div>
-            <div class='mb-5'>
-                <label for='phone' class='block text-gray-700 text-sm font-bold mb-2'>
-                    Phone
-                </label>
-                <input type='text' name='phone' id='phone' class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Phone'>
-            </div>
-            <div class='mb-5'>
-                <label for='date' class='block text-gray-700 text-sm font-bold mb-2'>
-                    Date
-                </label>
-                <input type='date' name='date' id='date' class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Date'>
-            </div>
-            <div class='mb-5'>
-                <label for='time' class='block text-gray-700 text-sm font-bold mb-2'>
-                    Time
-                </label>
-                <input type='time' name='time' id='time' class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Time'>
-            </div>
-        </form>
+            <h1 class='text-4xl my-5'>Request an Appointment</h1>
+            <form method='post' action=''>
+                @csrf
+                <div class='flex flex-col'>
+                    <label for='name' class='text-lg'>Name</label>
+                    <input type='text' name='name' id='name' class='w-full p-2 border border-black rounded-lg' required>
+                </div>
+                <div class='flex flex-col'>
+                    <label for='email' class='text-lg'>Email</label>
+                    <input type='email' name='email' id='email' class='w-full p-2 border border-black rounded-lg' required>
+                </div>
+                <div class='flex flex-col'>
+                    <label for='phone' class='text-lg'>Phone</label>
+                    <input type='tel' name='phone' id='phone' class='w-full p-2 border border-black rounded-lg' required>
+                </div>
+                <div class='flex flex-col'>
+                    <label for='date' class='text-lg'>Date</label>
+                    <input type='date' name='date' id='date' class='w-full p-2 border border-black rounded-lg' required>
+                </div>
+                <div class='flex flex-col'>
+                    <label for='time' class='text-lg'>Time</label>
+                    <input type='time' min='8:00' max='16:00' name='time' id='time' class='w-full p-2 border border-black rounded-lg' required>
+                </div>
+                <div class='flex flex-col'>
+                    <label for='message' class='text-lg'>Message</label>
+                    <textarea name='message' id='message' class='w-full p-2 border border-black rounded-lg' required></textarea>
+                </div>
+                <button type='submit' class='w-full p-2 mt-5 bg-blue-500 text-white rounded-lg'>Submit</button>
+            </form>
+        </div>
     </x-slot>
 </x-app-layout>
