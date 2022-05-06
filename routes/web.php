0db3344d4e5a588e22cc221b6dac335e7296adab
Gabe <gabecurran01@gmail.com>
2022-05-06 12:38:12 -0400

    Add email verification and routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Models\Home;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\MultipleUploadController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -69,6 +70,22 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::post('/image-upload', [MultipleUploadController::class, 'store'])->name('image-upload');
     Route::get('send-email', [App\Http\Controllers\EmailController::class, 'sendEmail']);
+
+    Route::get('/email/verify', function () {
+        return view('auth.verify-email');
+    })->middleware('auth')->name('verification.notice');
+ 
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+    
+        return redirect('/home');
+    })->middleware(['auth', 'signed'])->name('verification.verify');
+ 
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+    
+        return back()->with('message', 'Verification link sent!');
+    })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 });
 
 require __DIR__.'/auth.php'; 
